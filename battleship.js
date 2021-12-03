ccde7c1313aad31af0844fec6ccf1babb7a1c8db
KhaninPasha0488 <KhaninPasha0488>
2021-12-03 13:08:58 +0300

    03-12 modei.fire

--- battleship.js
+++ battleship.js
@@ -22,10 +22,40 @@ var model = {
    ships: [ {locations:["06","16","26"],hits: ["","",""]},
                    {locations:["24","34","44"],hits: ["","",""]},
                    {locations:["10","11","12"],hits: ["","",""]} ],
-   fire: function(guess){
+   fire: function(guess) {
       for (var i = 0; i < this.numShips; i++) {
          var ship = this.ships[i];
-         location = ship.location;
+         var index = ship.locations.indexOf(guess) ;
+         if ( index >= 0) {
+            ship.hits[index] = "hit" ;
+            view.displayHit(guess) ;
+            view.displayMessage("HIT!") ;
+            if (this.isSunk(ship)) {
+               view.displayMessage("You sank my battleship!") ;
+            }
+            return true ;
+         }
       }
+      view.displayMiss(guess);
+      view.displayMessage("You missed.") ;
+      return false ;
+   },
+   isSunk: function(ship) {
+      for (var i = 0; i< this.shipsLength; i++) {
+         if (ship.hits[i] !== "hit") {
+            return false
+         }
+      }
+      return true
    }               
 };
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
